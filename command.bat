e96a82f65c977c396c924f56a324856d423c3dc5
TheMakeDream <62189379+TheMakeDream@users.noreply.github.com>
2023-10-05 23:06:56 +0200

    Update command.bat

--- command.bat
+++ command.bat
@@ -1,25 +1,25 @@
 @echo off
-echo Choisissez une option:
-echo 1. Telecharger un flux avec Streamlink.
-echo 2. Convertir un fichier en mp4 (h265) avec FFmpeg.
-set /p choice="Entrez votre choix (1/2): "
+echo Choose an option:
+echo 1. Download a stream with Streamlink.
+echo 2. Convert a file to mp4 (h265) with FFmpeg.
+set /p choice="Enter your choice (1/2): "
 
 if "%choice%"=="1" goto Streamlink
 if "%choice%"=="2" goto FFmpeg
 goto end
 
 :Streamlink
-set /p streamURL="Entrez l'URL du flux a telecharger: "
+set /p streamURL="Enter the URL of the stream to download: "
 Program\bin\streamlink.exe --output "./VOD/{author}/[{time:%%Y}-{time:%%m}-{time:%%d}] {title} - {id}.ts" --twitch-disable-ads --stdout --ffmpeg-video-transcode h265 %streamURL% best
 goto end
 
 :FFmpeg
-echo Glissez le fichier a convertir ici et appuyez sur Entree:
+echo Drag the file to be converted here and press Enter:
 set /p inputPath=""
 for %%a in (%inputPath%) do set "outputFile=%%~dpa%%~na_converted.mp4"
 Program\ffmpeg\ffmpeg.exe -i %inputPath% -c:a copy "%outputFile%"
 goto end
 
 :end
-echo Operation terminee.
+echo Operation terminated.
 pause
